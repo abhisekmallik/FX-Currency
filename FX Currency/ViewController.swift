7790f40157e5d5ea09f434c47a94db8ef0fe4568
Abhisek Mallik <abhisek.mallik@outlook.com>
2016-09-04 22:32:00 +0400

    Initial Commit

---
+++ FX Currency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FX Currency
+//
+//  Created by Abhisek Mallik on 9/4/16.
+//  Copyright © 2016 Abhisek Mallik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
